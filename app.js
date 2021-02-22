ef700865b9fdf197297720d0b9a49adc1e4ba164
Colby <cabbott195@west-mec.org>
2021-02-22 16:27:29 -0700

    Added Model and Controller Files

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//cabbott195:J9ihPHDuSS2hZv3
+
+var mongoDB = '';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
