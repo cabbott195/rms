2605ab09419405084f47701499a68a9ffbd4d0cc
Dominic Torres <dtorres243@west-mec.org>
2021-02-22 17:12:40 -0700

    updated home.js

---
+++ routes/home.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+//Require controller modules
+var ir_controller = require('../controllers/irController');
+var viewir_controller = require('../controllers/viewirController');
